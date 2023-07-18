81b3f53f352090a808f3a81bbf53292f6d49af03
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2023-07-18 09:29:44 -0300

    Create dump_desafio_php.sql

---
+++ dump_desafio_php.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE desafioPHP;
+
+USE desafioPHP;
+
+CREATE TABLE textos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    texto TEXT
+);
+
+INSERT INTO textos (texto) VALUES ('A vida é muito curta para ser pequena. - Benjamin Disraeli');
+INSERT INTO textos (texto) VALUES ('Seja a mudança que você deseja ver no mundo. - Mahatma Gandhi');
+INSERT INTO textos (texto) VALUES ('A felicidade não está em fazer o que a gente quer, e sim querer o que a gente faz. - Jean Paul Sartre');
+INSERT INTO textos (texto) VALUES ('Nada é permanente, exceto a mudança. - Heráclito');
+INSERT INTO textos (texto) VALUES ('A imaginação é mais importante que o conhecimento. - Albert Einstein');
+INSERT INTO textos (texto) VALUES ('A vida é o que acontece enquanto você está ocupado fazendo outros planos. - John Lennon');
+INSERT INTO textos (texto) VALUES ('Não existe um caminho para a felicidade. A felicidade é o caminho. - Mahatma Gandhi');
+INSERT INTO textos (texto) VALUES ('O sucesso é ir de fracasso em fracasso sem perder entusiasmo. - Winston Churchill');
+INSERT INTO textos (texto) VALUES ('Não espere por uma crise para descobrir o que é importante em sua vida. - Platão');
+INSERT INTO textos (texto) VALUES ('A educação é a arma mais poderosa que você pode usar para mudar o mundo. - Nelson Mandela');
+INSERT INTO textos (texto) VALUES ('A persistência é o caminho do êxito. - Charles Chaplin');
+INSERT INTO textos (texto) VALUES ('O verdadeiro heroísmo consiste em persistir por mais um momento quando tudo parece perdido. - W. F. Grenfel');
+INSERT INTO textos (texto) VALUES ('A melhor maneira de prever o futuro é criá-lo. - Peter Drucker');
+INSERT INTO textos (texto) VALUES ('A única maneira de fazer um ótimo trabalho é amar o que você faz. - Steve Jobs');
+INSERT INTO textos (texto) VALUES ('A imaginação é mais importante que o conhecimento. - Albert Einstein');
+INSERT INTO textos (texto) VALUES ('A simplicidade é o último grau de sofisticação. - Leonardo da Vinci');
+INSERT INTO textos (texto) VALUES ('A arte de viver consiste em tirar o maior bem do maior mal. - Machado de Assis');
+INSERT INTO textos (texto) VALUES ('A liberdade é a possibilidade do isolamento. Se te é impossível viver só, nasceste escravo. - Fernando Pessoa');
+INSERT INTO textos (texto) VALUES ('O tempo dura bastante para aqueles que sabem aproveitá-lo. - Leonardo da Vinci');
+INSERT INTO textos (texto) VALUES ('A amizade duplica as alegrias e divide as tristezas. - Francis Bacon');
+INSERT INTO textos (texto) VALUES ('A vida é o que acontece quando você está ocupado fazendo outros planos. - John Lennon');
+INSERT INTO textos (texto) VALUES ('A arte de viver é simplesmente a arte de conviver. - Vinicius de Moraes');
+INSERT INTO textos (texto) VALUES ('Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado. - Roberto Shinyashiki');
+INSERT INTO textos (texto) VALUES ('Só se pode alcançar um grande êxito quando nos mantemos fiéis a nós mesmos. - Friedrich Nietzsche');
+INSERT INTO textos (texto) VALUES ('Nunca se afaste de seus sonhos, pois se eles se forem, você continuará vivendo, mas terá deixado de existir. - Charlie Chaplin');
